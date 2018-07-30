cb430bb30402d2926603f5b085b63c00f9cdbd3e
mballance-sf <matt.ballance@gmail.com>
2018-07-29 17:42:16 -0700

    Correct errors in truncator

--- rtl/adapters/wb_addr_trunc_adapter.sv
+++ rtl/adapters/wb_addr_trunc_adapter.sv
@@ -18,28 +18,28 @@ module wb_addr_trunc_adapter #(
 	generate 
 		if (WB_ADDR_PASSTHROUGH < WB_ADDR_WIDTH) begin
 			// The output is smaller than the input. Pad the output with zero
-			assign out.ADR[WB_ADDR_PASSTHROUGH-1:0] = in.ADR[WB_ADDR_PASSTHROUGH-1:0];
+			assign out.ADR[WB_ADDR_PASSTHROUGH-1:0] = i.ADR[WB_ADDR_PASSTHROUGH-1:0];
 			assign out.ADR[WB_ADDR_WIDTH-1:WB_ADDR_PASSTHROUGH] = 0;
 		end else begin
 			// Very boring... They're the same.
-			assign out.ADR = in.ADR;
+			assign out.ADR = i.ADR;
 		end
 	endgenerate
 	
-	assign out.TGA = in.TGA;
-	assign out.CTI = in.CTI;
-	assign out.BTE = in.BTE;
-	assign out.DAT_W = in.DAT_W;
-	assign out.TGD_W = in.TGD_W;
-	assign in.DAT_R = out.DAT_R;
-	assign out.TGD_R = in.TGD_R;
-	assign out.CYC = in.CYC;
-	assign out.TGC = in.TGC;
-	assign in.ERR = out.ERR;
-	assign out.SEL = in.SEL;
-	assign out.STB = in.STB;
-	assign in.ACK = out.ACK;
-	assign out.WE = in.WE;
+	assign out.TGA = i.TGA;
+	assign out.CTI = i.CTI;
+	assign out.BTE = i.BTE;
+	assign out.DAT_W = i.DAT_W;
+	assign out.TGD_W = i.TGD_W;
+	assign i.DAT_R = out.DAT_R;
+	assign out.TGD_R = i.TGD_R;
+	assign out.CYC = i.CYC;
+	assign out.TGC = i.TGC;
+	assign i.ERR = out.ERR;
+	assign out.SEL = i.SEL;
+	assign out.STB = i.STB;
+	assign i.ACK = out.ACK;
+	assign out.WE = i.WE;
 
 endmodule
 
